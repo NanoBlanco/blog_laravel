f1574ef4acc4460f7298ebc981c865173a7f5c05
= <=>
2025-09-18 15:55:21 -0300

    Primer Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+    <x-slot:title>
+        Home
+    </x-slot:title>
+    <div class="max-w-2x1 mx-auto">
+        <div class="card bg-base-100 shadow mt-8">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-3x1 font-bold">Welcome to Chirper! </h1>
+                    <p class="mt-4 text-base-content/60">Esta es tu nueva marca de aplicación Laravel. Es tiempo de
+                        hacerlo cantar</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
